9b41e8840d74a04555d0c547bfcf3a427e5440b6
Marat Nikolaev <gremlen.8149860@yandex.by>
2023-08-05 22:09:40 +0300

    refactor: delete test directorys

--- src/validateValue/validateValue.test.ts
+++
@@ -1,5 +0,0 @@
-import validate from './validateValue';
-
-test('Валидация', () => {
-	expect(validate(50)).toBe(true);
-});
--- src/validateValue/validateValue.ts
+++
@@ -1,8 +0,0 @@
-const validate = (num: number) => {
-	if (num < 0 || num > 100) {
-		return false;
-	}
-	return true;
-};
-
-export default validate;
