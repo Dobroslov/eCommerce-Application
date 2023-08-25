4e51cda81267a2d826d057ef30d0dfddc1e3a87b
Vladimir Arshinnikov <lordofrain7@gmail.com>
2023-08-25 12:58:04 +0300

    refactor: merge

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import style from './loginPage.module.scss';
 import useAuth from '../../hooks/useAuth';
@@ -56,19 +56,6 @@ function LoginPage(): React.ReactElement | null {
 			},
 		);
 	};
-
-	// useEffect(() => {
-	// 	if (user) {
-	// 		// Только если пользователь успешно авторизовался, тогда выполняйте редирект
-	// 		if (location.state?.from) {
-	// 			navigate(location.state.from, {
-	// 				replace: true,
-	// 			});
-	// 		} else {
-	// 			navigate('/account_page');
-	// 		}
-	// 	}
-	// }, [user, location.state, navigate]);
 
 	return (
 		<div className={style.login}>
