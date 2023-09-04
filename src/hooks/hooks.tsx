24568bd789e06d69c78329c19fb458f194f9df90
Marat Nikolaev <gremlen.8149860@yandex.by>
2023-09-04 20:57:59 +0300

    fix: fix merge errors

--- src/hooks/hooks.tsx
+++ src/hooks/hooks.tsx
@@ -1,16 +1,9 @@
 import { useEffect, RefObject } from 'react';
 
-const useOnClickOutside = (
-	ref: RefObject<HTMLDivElement>,
-	closeMenu: () => void,
-) => {
+const useOnClickOutside = (ref: RefObject<HTMLDivElement>, closeMenu: () => void) => {
 	useEffect(() => {
 		const close = (event: MouseEvent): void => {
-			if (
-				ref.current &&
-				event.target &&
-				ref.current.contains(event.target as Node)
-			) {
+			if (ref.current && event.target && ref.current.contains(event.target as Node)) {
 				return;
 			}
 			closeMenu();
