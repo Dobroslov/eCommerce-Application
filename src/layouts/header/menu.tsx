8507e112cac9b2e79123a395b0f9fd0241aae96b
Marat Nikolaev <gremlen.8149860@yandex.by>
2023-09-18 23:22:36 +0300

    feat: add search functional

--- src/layouts/header/menu.tsx
+++ src/layouts/header/menu.tsx
@@ -4,36 +4,47 @@ import { useSelector } from 'react-redux';
 import useOnClickOutside from '../../hooks/hooks';
 import header from './header.module.scss';
 import Navigation from './navigation';
-// import HeaderButton from '../../components/buttons/headerButton';
+import HeaderButton from '../../components/buttons/headerButton';
 import SearchForm from './searchForm';
 import { RootState } from '../../store/reducers';
 
 function Menu(): React.JSX.Element {
 	const [active, setToggle] = useState(false);
-	// const toggleSearch = ():void => {
-	// 	setToggle(!active);
-	// };
+	const [activeSearch, setactiveSearch] = useState(false);
+	const toggleSearch = (): void => {
+		setToggle(!active);
+		setactiveSearch(true);
+	};
 
 	const closeSearch = (): void => {
 		if (active) {
 			setToggle(!active);
+			setactiveSearch(false);
 		}
 	};
-	const quantity = useSelector((state:RootState) => state.data.cart?.quantity,
-	);
+	const quantity = useSelector((state: RootState) => state.data.cart?.quantity);
 	const node = useRef<HTMLDivElement>(null);
 	useOnClickOutside(node, (): void => closeSearch());
 	return (
 		<div className={header.menu} ref={node}>
 			<Navigation />
 			<div className={header.cards}>
-				{/* <HeaderButton area-label='Search' className={`${header.search} ${header.button__header}`} onClick={toggleSearch} /> */}
-				<Link to='/cart' className={`${header.basket} ${header.button__header}`}><span className={header.quantity}>{quantity}</span></Link>
+				<HeaderButton
+					area-label='Search'
+					className={`${header.search} ${header.button__header}`}
+					onClick={toggleSearch}
+				/>
+				<Link to='/cart' className={`${header.basket} ${header.button__header}`}>
+					<span className={header.quantity}>{quantity}</span>
+				</Link>
 				{/* <HeaderButton area-label='Basket' className={`${header.basket} ${header.button__header}`}><span className={header.quantity}>{quantity}</span></HeaderButton> */}
 				<Link to='/account_page' className={`${header.profile} ${header.button__header}`} />
 				{/* <HeaderButton area-label='Profile' className={`${header.profile} ${header.button__header}`} /> */}
 			</div>
-			<SearchForm className={active ? `${header.search__form} ${header.open}` : header.search__form} />
+			<SearchForm
+				activeSearch={activeSearch}
+				className={active ? `${header.search__form} ${header.open}` : header.search__form}
+			/>
 		</div>
 	);
 }
--- src/layouts/header/searchForm.tsx
+++ src/layouts/header/searchForm.tsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import header from './header.module.scss';
+import { IProduct } from '../../utils/types';
+import { getFilter } from '../../services/apiServices';
 
 interface Props {
 	className: string;
 	onClick?: () => void;
+	activeSearch: boolean;
 }
 function SearchForm({ ...props }: Props): React.JSX.Element {
+	const [products, setProducts] = useState<IProduct[]>([]);
+	const [search, setSearch] = useState<string>('');
+	const input = useRef<HTMLInputElement | null>(null);
+
+	useEffect(() => {
+		if (search.length > 0 && props.activeSearch) {
+			getFilter(100, 0, `&text.en-US=${search}`).then((data) => {
+				if (data) {
+					setProducts(data.productsArr);
+				}
+			});
+		} else {
+			if (input.current) {
+				input.current.value = '';
+			}
+			setSearch('');
+			getFilter(100, 0, '&text.en-US=""').then((data) => {
+				if (data) {
+					setProducts(data.productsArr);
+				}
+			});
+		}
+	}, [search, props.activeSearch]);
+
+	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value);
+	};
+
 	return (
 		<form {...props}>
 			<input
 				type='text'
 				className={header.search__input}
 				placeholder='Search'
+				onChange={handleSearch}
+				ref={input}
 			/>
-			<button
-				aria-label='text'
-				type='submit'
-				className={header.search__button}
-			/>
+			<button aria-label='text' type='submit' className={header.search__button} />
+			<div className={products.length > 0 ? header.list : header.listNone}>
+				{products?.map((product) => (
+					<Link key={product.id + 20} to={`/shop/${product.id}`}>
+						<div className={header.body}>
+							<div>
+								<img src={product.image} alt='product-logo' />
+							</div>
+							<div>
+								<p className={header.name}>{product.name}</p>
+								<p>
+									{product.discount !== 'NaN' ? (
+										<>
+											{product.discount} {product.currencyCode}{' '}
+											<p className={header.lineThrough}>
+												{product.price} {product.currencyCode}
+											</p>
+										</>
+									) : (
+										`${product.price} ${product.currencyCode}`
+									)}
+								</p>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
 		</form>
 	);
 }
